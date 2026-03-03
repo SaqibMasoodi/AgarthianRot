dae6ac82f603814c4bfa3526354553d5e57e61f3
Saqib Anjum Masoodi <88959559+SaqibMasoodi@users.noreply.github.com>
2026-03-03 17:47:53 +0530

    feat: add Firebase configuration and update .gitignore.

---
+++ js/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDKBHCZJbPY8dEGdQq0IP_HqviAC80qHho",
+    authDomain: "agarthianrot-blog.firebaseapp.com",
+    projectId: "agarthianrot-blog",
+    storageBucket: "agarthianrot-blog.firebasestorage.app",
+    messagingSenderId: "615096028279",
+    appId: "1:615096028279:web:8119b14b1ddcc67b33ca72"
+};
+
+export default firebaseConfig;
